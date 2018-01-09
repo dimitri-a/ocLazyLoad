d6852d9405becb273c1f57c429067dc9d6a26f3d
Dimitri <couwenberg@gmail.com>
2018-01-10 10:47:25 +1100

    simplified directive for testApp.js

--- examples/simpleExample/js/testApp.js
+++ examples/simpleExample/js/testApp.js
@@ -1,9 +1,13 @@
-angular.module("testApp", []).directive("sayHello", function() {
-  return {
-    scope: {
-      to: '@to'
-    },
-    restrict: "E",
-    template: '<p>Hello {{to}}</p>'
-  };
-});
+angular.module("testApp", [])
+    .directive("sayHello", function () {
+        return {
+            template: '<p>Hello this is a directive</p>'
+        };
+    });
+
+//
+// angular.module('LazyLoadTest',[])
+//     .controller('myCtrl',function($scope){
+//         console.log('myCtrl is loading');
+//         $scope.test ='hello this is lazyloaded!!';
+//     });
